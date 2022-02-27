dc1193f3cac9c09a78ba61d14610b13739bc701c
praveen-1995 <praveenkumarthipparam@gmail.com>
2022-02-26 18:31:17 -0600

    Implement bcrypt - Level 4 Security

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-//jshint esversion:6
 //We load the dotenv library and call the config method, which loads the variables into the process.env
-require("dotenv").config(); 
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -40,30 +41,38 @@ app.get("/login", function (req, res) {
 
 app.post("/register", function (req, res) {
 
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+
+        newUser.save(function (err) {
+            if (!err) {
+                res.render("secrets");
+            } else {
+                console.log(err);
+            }
+        });
     });
 
-    newUser.save(function (err) {
-        if (!err) {
-            res.render("secrets");
-        } else {
-            console.log(err);
-        }
-    });
 });
 
 app.post("/login", function (req, res) {
     const email = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({ email: email }, function (err, foundResult) {
         if (!err) {
             if (foundResult) {
-                if (foundResult.password === password) {
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, foundResult.password, function (err, result) {
+                    if (result === true) {
+                        res.render("secrets");
+                    } else {
+                        console.log("Please Try Again");
+                    }
+                });
             }
         } else {
             console.log(err);
